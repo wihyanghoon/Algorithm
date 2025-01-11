e1f7c5eb8ed3a8f59d56fe34633f60f7a5a62c5f
Wi-Hyanghoon <66665468+wihyanghoon@users.noreply.github.com>
2025-01-11 14:34:03 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    var answer = [0, 0];
+    
+    for (const num of num_list) {
+        if(num % 2 === 0) {
+            answer[0]++
+        } else {
+            answer[1]++
+        }
+    }
+    
+    return answer;
+}
