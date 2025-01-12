9ace10e97c9e42a8fcc27c4ed8fca97d3b6da17e
Wi-Hyanghoon <66665468+wihyanghoon@users.noreply.github.com>
2025-01-12 15:43:13 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,17 +1,4 @@
 function solution(s) {
-    //문자열에서 "Z" 를 만나면 "Z이전의 값을 빼준다"
-    let str = s.split(" ")
-    let result = 0;
-    
-    // 문자열을 순회
-    for(let i = 0; i < str.length; i++) {
-        // Z를 만날경우
-        if(str[i] === "Z") {
-            result -= parseInt(str[i-1])
-        } else {
-            result += parseInt(str[i])
-        }
-    }
-    
-    return result
+    var answer = 0;
+    return answer;
 }
